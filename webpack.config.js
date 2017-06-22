f155ca02438531b3dd15f807787e6e30b7193f6c
Victor Bello <Victor.Bello@ff.com>
2017-06-21 21:11:20 -0700

    Firebase deployment setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+const config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -21,3 +22,16 @@ module.exports = {
     template: 'app/index.html'
   })]
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
